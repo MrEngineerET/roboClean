0093670b5d39842d99455d87aa817e67b85f0420
Mr.Engineer Biruk <meetbirukberhanu@gmail.com>
2020-10-24 22:28:28 +0300

    speed sensor interrupt test code is added, speed sensor and geared motor datasheet is added

---
+++ Code/Arduino/interruptTest/interruptTest.ino
@@ -0,0 +1,44 @@
+// test interrupt pins with arduino
+
+int leftEncoder = 18; // Pin 18, where the left encoder pin DO is connected
+int rightEncoder = 19; // Pin 19, where the right ecoder pin DO is connected
+
+volatile int leftEncoderPulses = 0;   // Number of left Encoder pulses
+volatile int rightEncoderPulses = 0;  // Number of right Encoder pulses
+
+// variable used for reading a clean and good signal from the encoder
+volatile unsigned long debounceL = 0;   // time stamp of the last bounce of the incoming signal from the left encoder
+volatile unsigned long debounceR = 0;   // time stamp of the last bounce of the incoming signal from the right encoder
+
+unsigned long timeOld = 0;    // time stamp of the last serial output
+
+void setup () {
+   attachInterrupt (digitalPinToInterrupt(leftEncoder), LEncoder, RISING); // interrupt function: interrupt pin, function to call, type of activation
+   attachInterrupt (digitalPinToInterrupt(rightEncoder), REncoder, RISING); // interrupt function: interrupt pin, function to call, type of activation
+   Serial.begin (9600); // start of serial communication
+   Serial.println ("Hello From Encoder");
+
+}
+
+void loop () {
+  if(millis() - timeOld > 3000){
+      Serial.print("Left Count: ");
+      Serial.println(leftEncoderPulses);  
+      Serial.print("Right Count: ");
+      Serial.println(rightEncoderPulses);  
+      timeOld = millis();
+    }
+}
+
+void LEncoder () {// interrupt function of the left wheel encoder
+    if(micros()-debounceL > 500){
+        leftEncoderPulses++;
+        debounceL = micros();
+    }
+}
+void REncoder () {// interrupt function of the right wheel encoder
+   if(micros()-debounceR > 500){
+        rightEncoderPulses++;
+        debounceR = micros();
+    }
+}
