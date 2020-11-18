d751d2e6b52833bc97ecd36c7c1faceca4f46963
Mr.Engineer Biruk <meetbirukberhanu@gmail.com>
2020-11-18 11:54:14 +0300

    robotMotionSimulation version one on processing is implemented

--- Code/Matlab/position.m
+++ Code/Matlab/position.m
@@ -9,6 +9,9 @@ warning('off','MATLAB:serial:fscanf:unsuccessfulread');
 fopen(serial_port);
 
 figure
+title('Position of the Robot');
+xlabel('X axis(cm)');
+ylabel('Y axis(cm)');
 plot(0,0,'or');
 hold on;
 plot(100,100,'or');
